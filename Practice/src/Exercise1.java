b68420877b7443b107c5c4af250fe7b33f1af0d2
Ray Machado <nunumachado90@gmail.com>
2026-01-14 11:46:26 +0200

    Add files via upload

---
+++ Practice/src/Exercise1.java
@@ -0,0 +1,13 @@
+// We'll have to import the java.io library to acess it's data
+// The "io.*" star in quotes means that we'll be fetching all in this library
+
+// This is the class
+public class Exercise1 {
+
+    // This is the main method, where everything runs once you press the 'Run' button
+    public static void main(String[] args) {
+
+        // Print a message in the console
+        System.out.println("Hello World!");
+    }
+}
